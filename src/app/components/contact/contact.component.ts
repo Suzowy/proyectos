e7b0e7b972a205c6c16f1cf0d26eaa336d008079
Suzowy <suzowy@gmail.com>
2024-04-22 12:55:34 +0200

    cambios estilos

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -4,13 +4,13 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
-  styleUrl: './contact.component.css'
+  styleUrls: ['./contact.component.css']
 })
 export class ContactComponent implements OnInit {
   public widthSlider!: number;
   public anchuraToSlider!: any;
   public autor: any;
-
+  public email = 'suzowy@gmail.com';
   constructor() {
 
   }
