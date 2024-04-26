9c5c38895b10a9ba0b74da4e74aee95f7920162e
Suzowy <suzowy@gmail.com>
2024-04-26 13:35:50 +0200

    botones editar solo en local

--- src/app/components/detail/detail.component.ts
+++ src/app/components/detail/detail.component.ts
@@ -1,67 +1,63 @@
-
 import { Component, OnInit } from '@angular/core';
 import { Project } from '../../models/project';
 import { ProjectService } from '../../services/project.service';
 import { Global } from '../../services/global';
-import { Router, ActivatedRoute} from '@angular/router';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-detail',
   templateUrl: './detail.component.html',
-  styleUrl:'./detail.component.css',
+  styleUrls: ['./detail.component.css'],
   providers: [ProjectService]
 })
 export class DetailComponent implements OnInit {
   public url: string;
   public project!: Project;
   public confirm: boolean;
+  public isLocalMode: boolean; // Nueva variable para el modo local
 
   constructor(
-  	private _projectService: ProjectService,
-  	private _router: Router,
-  	private _route: ActivatedRoute
-  ){
-  	this.url = Global.url;
+    private _projectService: ProjectService,
+    private _router: Router,
+    private _route: ActivatedRoute
+  ) {
+    this.url = Global.url;
     this.confirm = false;
+    this.isLocalMode = true; // Establece según sea necesario
   }
 
+  setConfirm(confirm: boolean) {
+    this.confirm = confirm;
+  }
 
-  getProject(id: any) {
-    this._projectService.getProject(id).subscribe(
+  deleteProject(id: string) {
+    this._projectService.deleteProject(id).subscribe(
       response => {
-        this.project = response.project
-
+        if (response.project) {
+          this._router.navigate(['/proyectos']);
+        }
       },
-      (error:any) => {
-        console.log(error);
-
+      error => {
+        console.log(<any>error);
       }
     );
   }
 
-   ngOnInit(): void{
-  	this._route.params.subscribe(params => {
-  		let id = params['id'];
-
-  		this.getProject(id);
-  	});
+  getProject(id: any) {
+    this._projectService.getProject(id).subscribe(
+      response => {
+        this.project = response.project;
+      },
+      error => {
+        console.log(error);
+      }
+    );
   }
 
-  setConfirm(confirm: boolean){
-    this.confirm = confirm;
+  ngOnInit(): void {
+    this._route.params.subscribe(params => {
+      let id = params['id'];
+      this.getProject(id);
+    });
   }
-
-  deleteProject(id: string){
-  	this._projectService.deleteProject(id).subscribe(
-  		response => {
-  			if(response.project){
-  				this._router.navigate(['/proyectos']);
-  			}
-  		},
-  		error => {
-  			console.log(<any>error);
-  		}
-  	);
-  }
-
 }
